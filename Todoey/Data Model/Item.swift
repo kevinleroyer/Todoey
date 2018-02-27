54cc31099f03eeee1a466c79c1a233f715b69068
Kevin Leroyer <kevin.leroyer@gmail.com>
2018-02-27 09:33:27 +0100

    Persisting data using Codable protocol and plist

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Kévin Leroyer on 18-02-26.
+//  Copyright © 2018 Kévin Leroyer. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title : String = ""
+    var done : Bool = false
+}
